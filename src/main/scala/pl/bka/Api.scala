4af280880255302b16d6c4fcbdab5539e6d950ba
Bartek Kalinka <bartkali@hotmail.com>
2016-03-31 14:06:28 +0200

    returns tuples instead of map of maps

--- src/main/scala/pl/bka/Api.scala
+++ src/main/scala/pl/bka/Api.scala
@@ -3,9 +3,10 @@ package pl.bka
 case class Hash(value: String)
 case class Commit(hash: Hash)
 case class Word(value: String)
+case class WordCount(commit: Commit, word: Word, count: Int)
 
 object Api {
-  def read(path: String, take: Int = 10): Map[Commit, Map[Word, Int]] =
+  def read(path: String, take: Int = 10): Seq[WordCount] =
     Counter(RealInput(path, ".scala")).read(take)
 
   def write(data: Map[String, Map[String, Int]], outputPath: String) =
--- src/main/scala/pl/bka/Counter.scala
+++ src/main/scala/pl/bka/Counter.scala
@@ -1,17 +1,19 @@
 package pl.bka
 
 case class Counter(input: Input) {
-  def read(take: Int = 10): Map[Commit, Map[Word, Int]] = {
-    def wc(lines: Seq[LineOfText]): Map[Word, Int] =
+  def read(take: Int = 10): Seq[WordCount] = {
+    def wc(lines: Seq[LineOfText]): Seq[(Word, Int)] =
       lines
         .flatMap(_.value.split("\\W+"))
         .filterNot(_ == "")
         .groupBy(Word)
-        .mapValues(_.size)
+        .mapValues(_.size).toSeq
     try {
-      input.listCommits.take(take).map(commit =>
-        (commit, wc(input.checkout(commit.hash)))
-      ).toMap
+      input.listCommits.take(take).flatMap { commit =>
+        wc(input.checkout(commit.hash)).map { case (word, count) =>
+          WordCount(commit, word, count)
+        }
+      }
     }
     finally {
       input.cleanup
