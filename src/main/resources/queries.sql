49d59068339ad7ecbf556d98a58f6be690dc05ff
Bartek Kalinka <bartkali@hotmail.com>
2016-04-01 18:17:42 +0200

    queries.sql

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,7 @@
+﻿select * from wordcounts;
+
+
+select hash, '.' || word || '.', count from wordcounts order by count desc, hash, word;
+
+WITH agr AS (select word, max(count) maxcnt, min(count) mincnt from wordcounts group by word)
+select word, maxcnt, mincnt from agr where maxcnt - mincnt = (select max(maxcnt - mincnt) from agr);
