48193bc4dd953f8cd58617606a93c3950bc2f01b
Bartek Kalinka <bartkali@hotmail.com>
2016-04-06 12:52:12 +0200

    fix tests

--- src/test/scala/pl/bka/CounterSpec.scala
+++ src/test/scala/pl/bka/CounterSpec.scala
@@ -3,18 +3,16 @@ package pl.bka
 import org.scalatest._
 
 case object StubInput extends Input {
-  def listCommits: Seq[Commit] = Seq(Commit(Hash("2kc98sdf")))
+  def listCommits: Seq[Commit] = Seq(Commit(1, Hash("2kc98sdf")))
   def checkout(hash: Hash) = Seq(LineOfText("abcabc"), LineOfText("eghegh"))
   def cleanup = ()
 }
 
 class CounterSpec extends FlatSpec with Matchers {
   it should "correctly calculate wordcounts" in {
-    Counter(StubInput).read() should be (Map(
-      Commit(Hash("2kc98sdf")) -> Map(
-        Word("abcabc") -> 1,
-        Word("eghegh") -> 1
-       )
+    Counter(StubInput).read() should be (Seq(
+      WordCount(Commit(1, Hash("2kc98sdf")), Word("abcabc"), 1),
+      WordCount(Commit(1, Hash("2kc98sdf")), Word("eghegh"), 1)
     ))
   }
 }
