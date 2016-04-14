e71aeb7efdd234b0426f1add7ae95a57552757ca
Bartek Kalinka <bartkali@hotmail.com>
2016-04-14 13:39:43 +0200

    read from min commit

--- src/main/scala/pl/bka/Api.scala
+++ src/main/scala/pl/bka/Api.scala
@@ -4,6 +4,7 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.language.postfixOps
 import org.joda.time.DateTime
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class Hash(value: String)
 case class Commit(seqNum: Int, hash: Hash, date: DateTime)
@@ -12,7 +13,11 @@ case class WordCount(commit: Commit, word: Word, count: Int)
 
 object Api {
   def read(path: String, take: Int = 10): Seq[WordCount] =
-    Counter(RealInput(path, ".scala")).read(take)
+    Await.result(
+      for {
+        min <- Db.minCommit
+      } yield Counter(RealInput(path, ".scala")).read(min, take)
+    , Duration.Inf)
 
   def write(data: Seq[WordCount]) =
     Await.result(Db.write(data), Duration.Inf)
--- src/main/scala/pl/bka/Counter.scala
+++ src/main/scala/pl/bka/Counter.scala
@@ -1,15 +1,17 @@
 package pl.bka
 
 case class Counter(input: Input) {
-  def read(take: Int = 10): Seq[WordCount] = {
+  def read(startCommit: Option[Commit], take: Int = 10): Seq[WordCount] = {
     def wc(lines: Seq[LineOfText]): Seq[(Word, Int)] =
       lines
         .flatMap(_.value.split("\\W+"))
         .filterNot(_ == "")
         .groupBy(Word)
         .mapValues(_.size).toSeq
+    def filteredCommits: Seq[Commit] =
+      input.listCommits.dropWhile(x => x != startCommit.getOrElse(x)).tail.take(take)
     try {
-      input.listCommits.take(take).flatMap { commit =>
+      filteredCommits.flatMap { commit =>
         wc(input.checkout(commit.hash)).map { case (word, count) =>
           WordCount(commit, word, count)
         }
