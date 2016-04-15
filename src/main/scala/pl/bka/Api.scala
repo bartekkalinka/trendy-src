3b9367cf5d03af4c07581ae2509768e17c134b0f
Bartek Kalinka <bartkali@hotmail.com>
2016-04-16 00:12:37 +0200

    hasNext + load apis

--- src/main/scala/pl/bka/Api.scala
+++ src/main/scala/pl/bka/Api.scala
@@ -12,15 +12,27 @@ case class Word(value: String)
 case class WordCount(commit: Commit, word: Word, count: Int)
 
 object Api {
-  def read(path: String, take: Int = 10): Seq[WordCount] =
+  private val extension = ".scala"
+
+  def hasNext: Boolean =
     Await.result(
       for {
         min <- Db.minCommit
-      } yield Counter(RealInput(path, ".scala")).read(min, take)
+      } yield min.map(minCommit => minCommit.seqNum > 1).getOrElse(true)
+      , Duration.Inf)
+
+  def readNext(path: String, take: Int = 10): Seq[WordCount] =
+    Await.result(
+      for {
+        min <- Db.minCommit
+      } yield Counter(RealInput(path, extension)).read(min, take)
     , Duration.Inf)
 
-  def write(data: Seq[WordCount]) =
-    Await.result(Db.write(data), Duration.Inf)
+  def write(data: Seq[WordCount]) = Await.result(Db.write(data), Duration.Inf)
+
+  def loadNext(path: String, take: Int = 10) = write(readNext(path, take))
+
+  def loadAll(path: String, chunkSize: Int) = while(hasNext) loadNext(path, chunkSize)
 
   def clearDb() = Await.result(Db.delete, Duration.Inf)
 }
